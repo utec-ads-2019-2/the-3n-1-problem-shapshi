ab83ddd63a82b9d6f84a158e23bef72a207ce15a
shapshi <54108619+shapshi@users.noreply.github.com>
2019-08-21 21:04:09 -0500

    Source code de la solución de p100

--- main.cpp
+++ main.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include<iostream>
+using namespace std; 
 
-using namespace std;
+int main(){
+	int n,m;	
+	cin>>n;
+	cin>>m;
+	
+	int tmp=0;
+	int cont;
+	int ICopy;
 
-int main(int argc, char *argv[]) {
-    return 0;
+	for(int i=n;i<=m;i++){
+            cont=1;   
+	    ICopy=i;
+            while(ICopy!=1){
+                 if(ICopy%2!=0)
+                         ICopy=ICopy*3+1;
+                 else
+                         ICopy=ICopy/2;
+                 
+	     	cont=cont+1;
+         	}
+	    if(cont>tmp){
+	    	tmp=cont;
+	    }
+	     
+	}
+
+
+	cout<<n<<" "<<m<<" "<<tmp<<endl;
+
+
+	return 0; 
 }
